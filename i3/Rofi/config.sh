7858ba6e1a02db96a6521ca45b2bf9f342b9e4f3
JasonLong24 <jasonlongball@gmail.com>
2018-04-11 21:39:08 +0000

    Changing configs for arch

--- i3/Rofi/config.sh
+++ i3/Rofi/config.sh
@@ -3,31 +3,31 @@
 res=$(echo "urls|add-on|vim|vimrc|polybar|zshrc|Xresources|i3config|theme|rofi" | rofi -sep "|" -dmenu -i -p 'Window Options: ' "")
 
 if [ $res = "vimrc" ]; then
-	exec urxvt -e vim ~/.vimrc
+	exec xterm -e vim ~/.vimrc
 
 elif [ $res = "polybar" ]; then
-	exec urxvt -e vim ~/i3wm/polybar/config
+	exec xterm -e vim ~/i3wm/polybar/config
 
 elif [ $res = "zshrc" ]; then
-	exec urxvt -e vim ~/i3wm/zsh/zshrc
+	exec xterm -e vim ~/i3wm/zsh/zshrc
 
 elif [ $res = ".Xresources" ]; then
-	exec urxvt -e vim ~/.xresources
+	exec xterm -e vim ~/.xresources
 
 elif [ $res = "i3config" ]; then
-	exec urxvt -e vim ~/i3wm/i3/config
+	exec xterm -e vim ~/i3wm/i3/config
 
 elif [ $res = "theme" ]; then
-	exec urxvt -e vim /home/jason/.oh-my-zsh/themes/jasonzsh.zsh-theme
+	exec xterm -e vim /home/jason/.oh-my-zsh/themes/jasonzsh.zsh-theme
 
 elif [ $res = "vim" ]; then
-	exec urxvt -e vim  
+	exec xterm -e vim  
 elif [ $res = "rofi" ]; then
-	exec urxvt -e vim +NERDTree ~/i3wm/i3/Rofi +q
+	exec xterm -e vim +NERDTree ~/i3wm/i3/Rofi +q
 
 elif [ $res = "add-on" ]; then
-  exec urxvt -e vim +NERDTree ~/Github/vim-nav-firefox +q
+  exec xterm -e vim +NERDTree ~/Github/vim-nav-firefox +q
 
 elif [ $res = "urls" ]; then
-  exec urxvt -e vim ~/i3wm/newsbeuter/urls
+  exec xterm -e vim ~/i3wm/newsbeuter/urls
 fi
