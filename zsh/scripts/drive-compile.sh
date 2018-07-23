8bed25d5dac84b8cbf5acab509cfec1ac8e3642e
JasonLong24 <jasonlongball@gmail.com>
2018-07-22 20:42:04 -0400

    build pdfs with file structure

--- zsh/scripts/drive-compile.sh
+++ zsh/scripts/drive-compile.sh
@@ -18,13 +18,44 @@ function onDirPick() {
   fi
 }
 
-if [[ $1 = "" ]]; then
+function compile() {
+  if [[ $dirPick = "0" ]]; then
+    for (( i=1;i<=$dirCount;i++ )); do
+      currentDir=$(echo "$availDirs" | awk NR==$i)
+      fileCount=$(ls -1q $(echo "$availDirs" | awk NR==$i)/*.md | wc -l)
+      files=$(ls -1q $(echo "$availDirs" | awk NR==$i)/*.md)
+      for (( j=1;j<=$fileCount;j++ )); do
+        pandoc --verbose $(echo "$files" | awk NR==$j) --output build/$(echo "$files" | awk NR==$j | cut -f 1 -d '.').pdf
+      done
+    done
+  elif [[ $dirPick =~ ^[1-9]+$ ]]; then
+    fileCount=$(ls -1q $(echo "$availDirs" | awk NR==$dirPick)/*.md | wc -l)
+    files=$(ls -1q $(echo "$availDirs" | awk NR==$dirPick)/*.md)
+    for (( i=1;i<=$fileCount;i++ )); do
+      pandoc --verbose $(echo "$files" | awk NR==$i) --output build/$(echo "$files" | awk NR==$i | cut -f 1 -d '.').pdf
+    done
+  fi
+}
+
+function start() {
   if [[ -f .drivecompile ]] && [[ -d build ]]; then
-    listDirs
+    if [[ $all = 0 ]]; then
+      listDirs
+    fi 
     onDirPick
+    compile
+    echo -e "\nBuild finished"
   else
     echo This Directory is not initialized.
   fi
+}
+
+if [[ $1 = "" ]]; then
+  all=0
+  start
+elif [[ $1 = "--all" ]] || [[ $1 = "-a" ]]; then
+  dirPick=0
+  start
 elif [[ $1 = "init" ]]; then
   if [[ -f .drivecompile ]] && [[ -d build ]]; then
     echo This Directory is already initialized.
