2ea5538e9bb735082ae76c7aa99eb63c70e14849
JasonLong24 <jasonlongball@gmail.com>
2018-09-23 21:08:49 -0400

    font and lines in dmenu

--- i3/Scripts/dmenu.sh
+++ i3/Scripts/dmenu.sh
@@ -3,6 +3,8 @@
 prompt=$(echo Run:)
 back=$(echo \#000000)
 fore=$(echo \#cc0000)
-selback=$(echo \#333)
+selback=$(echo \#000000)
 selfore=$(echo \#ffb52a)
-dmenu_run -b -p $prompt -nb $back -nf $fore -sb $selback -sf $selfore
+font=$(echo \'terminus:pixelsize=13:antialias=true\')
+lines=$(echo 15)
+dmenu_run -b -p $prompt -nb $back -nf $fore -sb $selback -sf $selfore -l $lines -fn $font
