56e20c798876d4cdc387a82bbfb8c6e0c0c3dffe
JasonLong24 <jasonlongball@gmail.com>
2018-12-28 20:25:18 -0500

    change sign color on exit code

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,40 +1,29 @@
 function precmd {
-  AltPrompt=null
+
+  if [ $? -eq 0  ]; then
+    END="$PROMPTC$ "
+  else
+    END="$ALTPROMPT$ "
+  fi
+
+  function get_sudo() {
+    if sudo -n true 2>/dev/null; then
+      echo "sudo"
+    else
+      echo ""
+    fi
+  }
+
   BRACKET="%{%F{white}%}"
   SUDO="%{%F{red}%}"
   DIR="%{%F{cyan}%}"
-  PROMPT="%{%F{white}%}"
+  PROMPTC="%{%F{white}%}"
   USER="%{%F{yellow}%}"
   GIT_BRANCH="%{%F{magenta}%}"
   ALTPROMPT="%{%F{red}%}"
 
-  # Functions# {{{
-  function get_sudo() {
-    if sudo -n true 2>/dev/null; then 
-      echo "sudo" 
-    else
-      echo "" 
-    fi
-  }
-
-  case $AltPrompt in
-    tmux)
-      session="("$(tmux ls | wc -l)")" ;;
-    cpu)
-      session="("$(top -bn 1 | grep "Cpu" | awk '{print $2}')")" ;;
-    mem)
-      session="("$(free -m | awk 'NR==2{print $3}')")" ;;
-    hist)
-      session="("$(history | cut -d " " -f4-15 | tail -1)")" ;;
-    hist-num)
-      session"("$(history | cut -d " " -f2)")" ;;
-    *)
-      session="" ;;
-  esac
-
   DIR="[$DIR%B%(5~|../%3~|%~)%b$BRACKET$BRACKET]% "
   USER="$BRACKET@$USER%m"
-  END="$PROMPT$ "
 
   PROMPT="$SUDO$(get_sudo)$USER$BRACKET$DIR$END%{$reset_color%}%"
   RPROMPT="$(git_full_prompt)"
