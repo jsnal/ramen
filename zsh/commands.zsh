ba4e2d90cd738f76adc9bbb21798db904937652a
jsnal <jasonlongball@gmail.com>
2019-04-24 20:17:06 -0400

    fix some zsh things

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -159,7 +159,7 @@ function paste-send() {
   local SEND_OUTPUT_HOST="jasonlong24.crabdance.com"
   local SEND_OUTPUT_PORT="2757"
   if [ ! -f $1 ]; then echo "$1 is not a valid file."; return 1; fi
-  read -p "Please enter a title: " entryTitle && read -p "Please enter the date: " entryDate && read -p "Please enter a filetype: " entryFT
+  read "?Please enter a title: " entryTitle && read "?Please enter the date: " entryDate && read "?Please enter a filetype: " entryFT
   echo -e "{{*title: $entryTitle\n{{*date: $entryDate\n{{*filetype: $entryFT" | cat - $1 | nc -v -q 0 $SEND_OUTPUT_HOST $SEND_OUTPUT_PORT
   echo "File Sent!"
 }
