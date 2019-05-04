2c28a7671df0407cfbee9b142d2a1f8d412aa629
jsnal <jasonlongball@gmail.com>
2019-05-03 22:28:07 -0400

    tweaking tmux config

--- i3/Scripts/tmux.sh
+++ i3/Scripts/tmux.sh
@@ -2,8 +2,5 @@ tmux kill-server
 tmux att -t get ||
 tmux -2 \
 	new \; \
-	neww -n 'Telegram' ~/tg/bin/telegram-cli -W -N\; \
-  neww ncmpcpp\; \
-  neww -n 'TODO' vim ~/i3wm/TODO.txt +":hi clear SpellBad | hi SpellBad cterm=underline"\; \
-	neww alsamixer\; \
+	neww -n 'volume' alsamixer\; \
 	selectw -t 0\; \
