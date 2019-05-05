9cf92951d955f1c543cc9967ace237b1cd1ecbd0
jsnal <jasonlongball@gmail.com>
2019-05-05 18:52:19 -0400

    fix some compatibility issues

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -60,4 +60,3 @@ alias d="source $HOME/i3wm/zsh/plugins/shell-plugins/zshbookmarks/zshbookmark.sh
 alias f="source $HOME/i3wm/zsh/plugins/shell-plugins/zshbookmarks/zshbookmark.sh -c $HOME/i3wm/zsh/.filebookmarks file"
 alias stagit-gen="$HOME/i3wm/zsh/plugins/shell-plugins/stagit-gen/gen.sh"
 alias qp="git add --all && git commit  && git push $1 $(git branch 2>/dev/null | grep \* | sed -e 's/*[[:space:]]//g')"
-alias xmod-caps="xmodmap $HOME/.Xmodmap"
