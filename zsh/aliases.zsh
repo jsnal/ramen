8c9505ef653d335cf8e4b7fe1d61e4fefb4ad215
jsnal <jasonlongball@gmail.com>
2019-07-17 15:03:53 -0400

    some new gitconfig and zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,12 +21,10 @@ alias z='zathura'
 alias ls='ls -Fh --color=always --group-directories'
 alias ll='ls -hl --color=always --group-directories'
 alias l='ls'
+alias cp='cp -v'
+alias trash='mv -t /tmp/'
 alias aa='sudo $(fc -ln -1)'
 alias gotop='gotop --color=monokai'
-alias tll='~/i3wm/i3/Scripts/tll.sh l'
-alias tlc='~/i3wm/i3/Scripts/tll.sh c "$@"'
-alias tla='~/i3wm/i3/Scripts/tll.sh a "$@"'
-alias drivec="~/i3wm/zsh/scripts/drive-compile.sh"
 alias myip="ip -color address"
 alias crontab-all="crontab -l | grep -v '^#' | cut -f 6- -d ' ' | while read CMD; do eval $CMD; done"
 alias bat="cat /sys/class/power_supply/BAT0/capacity"
@@ -38,3 +36,7 @@ alias f="source $HOME/i3wm/zsh/plugins/shell-plugins/zshbookmarks/zshbookmark.sh
 alias stagit-gen="$HOME/i3wm/zsh/plugins/shell-plugins/stagit-gen/gen.sh"
 alias qp="git add --all && git commit  && git push $1 $(git branch 2>/dev/null | grep \* | sed -e 's/*[[:space:]]//g')"
 alias paste-search="wget -qO- jasonlong24.crabdance.com/paste/posts.lst | sed 1,5d | grep -i "$1""
+alias tll='~/i3wm/i3/Scripts/tll.sh l'
+alias tlc='~/i3wm/i3/Scripts/tll.sh c "$@"'
+alias tla='~/i3wm/i3/Scripts/tll.sh a "$@"'
+alias drivec="~/i3wm/zsh/scripts/drive-compile.sh"
