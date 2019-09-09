f54bce46e399be2384a3949bad2786e2511f6bcd
jsnal <jasonlongball@gmail.com>
2019-09-09 10:16:50 -0400

    remove linux specific ls alias and update paste-search for new domain

--- zsh/startup/aliases.zsh
+++ zsh/startup/aliases.zsh
@@ -5,6 +5,7 @@ alias yaup='yay -Sy && sudo abs && sudo aur'
 alias yals='yay -Qi'
 alias yamir='yay -Syy'
 
+# Short tmux
 alias tmuxn='tmux -2 new -s "$@"'
 alias tmuxk='tmux kill-session -t "$@"'
 alias tmuxa='tmux -2 a -t "$@"'
@@ -18,8 +19,8 @@ alias ..5='cd ../../../../..'
 
 alias v='vim'
 alias z='zathura'
-alias ls='ls -Fh --color=always --group-directories'
-alias ll='ls -hl --color=always --group-directories'
+alias ls='ls --color=always --group-directories'
+alias ll='ls --color=always --group-directories'
 alias l='ls'
 alias cp='cp -v'
 alias trash='mv -t /tmp/'
@@ -28,5 +29,5 @@ alias gotop='gotop --color=monokai'
 alias myip="ip -color address"
 alias crontab-all="crontab -l | grep -v '^#' | cut -f 6- -d ' ' | while read CMD; do eval $CMD; done"
 alias j="jump"
-alias paste-search="wget -qO- jasonlong24.crabdance.com/paste/posts.lst | sed 1,5d | grep -i "$1""
+alias paste-search="wget -qO- jasonlong.xyz/paste/posts.md | sed 1,5d | grep -i "$1""
 alias test_color="printf '\e[48;5;%dm ' {0..255}; printf '\e[0m \n'"
