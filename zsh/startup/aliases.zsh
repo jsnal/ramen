0b852ec5029f38e29a4b6e8f7f2efa18f2619a79
jsnal <jasonlongball@gmail.com>
2019-12-13 09:19:09 -0500

    zsh: fix the paste_search alias
    Also reorganize the aliases.zsh file just a little bit.

--- zsh/startup/aliases.zsh
+++ zsh/startup/aliases.zsh
@@ -17,19 +17,21 @@ alias ..3='cd ../../..'
 alias ..4='cd ../../../..'
 alias ..5='cd ../../../../..'
 
+# System command aliases
 alias v='vim'
 alias vi='vim'
 alias z='zathura'
+alias j="jump"
 alias ls='ls --color=always --group-directories'
 alias ll='ls --color=always --group-directories'
 alias l='ls'
 alias cp='cp -v'
 alias trash='mv -t /tmp/'
 alias aa='sudo $(fc -ln -1)'
-alias gotop='gotop --color=monokai'
 alias myip="ip -color address"
-alias crontab-all="crontab -l | grep -v '^#' | cut -f 6- -d ' ' | while read CMD; do eval $CMD; done"
-alias j="jump"
-alias paste-search="wget -qO- jasonlong.xyz/paste/posts.md | sed 1,5d | grep -i "$1""
+
+# Improvement of life commands
+alias crontab_all="crontab -l | grep -v '^#' | cut -f 6- -d ' ' | while read CMD; do eval $CMD; done"
+alias paste_search='wget -qO- jasonlong.xyz/paste/posts.list | sed 1,5d | grep -i "$@"'
 alias test_color="printf '\e[48;5;%dm ' {0..255}; printf '\e[0m \n'"
 alias config_version="git --git-dir ~/i3wm/.git rev-parse HEAD"
