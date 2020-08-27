347f0bcaf78d6877d8494f7a5149ada3b1fa5fb2
jsnal <jasonlongball@gmail.com>
2020-08-26 21:38:36 -0400

    zsh: fix prompt timestamp
    Yeah so I thought I fixed this in 6fedaa0 but I guess I just broke it
    whoops.

--- zsh/startup/prompt.zsh
+++ zsh/startup/prompt.zsh
@@ -48,15 +48,15 @@ zsh_command_time() {
     if [ "$ZSH_COMMAND_TIME" -le 1 ]; then
       _ramen[COMMAND_TIME]=""
     elif [ "$ZSH_COMMAND_TIME" -le 60 ]; then
-      _ramen[COMMAND_TIME]="${ZSH_COMMAND_TIME}s"
+      _ramen[COMMAND_TIME]="${ZSH_COMMAND_TIME}s "
     elif [ "$ZSH_COMMAND_TIME" -gt 60 ] && [ "$ZSH_COMMAND_TIME" -le 180 ]; then
-      _ramen[COMMAND_TIME]="${min}min ${sec}s"
+      _ramen[COMMAND_TIME]="${min}min ${sec}s "
     else
       if [ "$hours" -gt 0 ]; then
         min=$(($min%60))
-        _ramen[COMMAND_TIME]="%B$_ramen[RED]${hours}h%b$_ramen[GRAY] ${min}min ${sec}s"
+        _ramen[COMMAND_TIME]="%B$_ramen[RED]${hours}h%b$_ramen[GRAY] ${min}min ${sec}s "
       else
-        _ramen[COMMAND_TIME]="${min}min ${sec}s"
+        _ramen[COMMAND_TIME]="${min}min ${sec}s "
       fi
     fi
   fi
