85bec497e45b80d1698c1f4b97523a12f86cc515
Jason Long <jasonlongball@gmail.com>
2023-11-02 10:16:04 -0400

    vim: remove LuaSnip as a vim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -137,20 +137,12 @@ require('nvim-rooter').setup()
 
 -- Completion engine
 local cmp = require('cmp')
-local luasnip = require('luasnip')
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
     mapping = {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
             else
                 fallback()
             end
@@ -158,8 +150,6 @@ cmp.setup {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
             else
                 fallback()
             end
@@ -185,6 +175,9 @@ end
 
 -- Turn off inline errors
 vim.diagnostic.config({ virtual_text = false })
+
+-- Turn off code snippets
+capabilities.textDocument.completion.completionItem.snippetSupport = false
 
 -------------------------------------------------------------------------------
 -- Auto Commands {{{1 ---------------------------------------------------------
