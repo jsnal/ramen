514b4cd8fef036b82ef1ebfeedc7262c7cb677e2
Jason Long <jasonlongball@gmail.com>
2024-03-19 21:14:21 -0400

    vim: revive old vim configuration for easy install

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ ENDL="\033[0m"
 #   1. Check if the repository exists locally; If it doesn't clone it.
 #   2. Notify the user of the current git commit hash they are about to install.
 #   3. Verify all the project files are present before starting.
-#   4. Finally get the selected profile and install the files accordingly.
+#   4. Perform the install.
 #
 echo -e "${UNDR}Cloning ramen${ENDL}"
 
@@ -35,69 +35,16 @@ else
   git clone --recursive --quiet https://github.com/jsnal/ramen $DOTFILES_DIR &>/dev/null
 fi
 
-# Globals
-PROFILES=("all" "desktop" "terminal")
-PROFILE_SELECT="terminal"
-DESKTOP_LIST=(
-  "$DOTFILES_DIR/dunst/dunstrc:$HOME/.config/dunst/dunstrc" \
-  "$DOTFILES_DIR/X/xresources:$HOME/.Xresources"            \
-  "$DOTFILES_DIR/X/xbindkeysrc:$HOME/.xbindkeysrc"          \
-  "$DOTFILES_DIR/X/xmodmap:$HOME/.Xmodmap"                  \
-)
-TERMINAL_LIST=(
+# List of files to install
+FILE_LIST=(
   "$DOTFILES_DIR/zsh/zshenv:$HOME/.zshenv"                 \
   "$DOTFILES_DIR/zsh/zshrc:$HOME/.zshrc"                   \
-  "$DOTFILES_DIR/X/xresources:$HOME/.Xresources"           \
   "$DOTFILES_DIR/tmux/tmux.conf:$HOME/.tmux.conf"          \
-  "$DOTFILES_DIR/nvim:$HOME/.config/nvim"                  \
+  "$DOTFILES_DIR/vim:$HOME/.vim"                           \
 )
 
-# Usage information
-function usage() {
-  echo './install.sh [OPTIONS]'
-  echo -e '\nOptions:'
-  echo -e '  -p, --profile [PROFILE] \tChange the install profile. Default terminal'
-  echo -e '\nProfiles:'
-  echo -e '  all      \tInstalls configuration files for desktop and terminal'
-  echo -e '  desktop  \tInstalls just configuration files for desktop'
-  echo -e '  terminal \tInstalls just configuration files for terminal'
-  exit
-}
-
-# Parse command line arguments
-POSITIONAL=()
-while [[ $# -gt 0 ]]; do
-  key="$1"
-  case $key in
-    -p|--profile)     PROFILE_SELECT=$2; shift ;;
-    -h|--help)        usage;             shift ;;
-    *)
-      POSITIONAL+=("$1"); shift ;;
-  esac
-done
-set -- "${POSITIONAL[@]}"
-
-# Symlink a file to a destination
-function link-file() {
-  ln -sfv $1 $2 | sed "s/'//g"
-}
-
-# Given an array link the file to a destination
-function link-file-list() {
-  local file_list=$1[@]
-  local file_list=("${!file_list}")
-
-  for file in "${file_list[@]}"; do
-    local source="${file%%:*}"
-    local destination="${file##*:}"
-
-    [ -d $source ] && mkdir -p $destination && local source="$source/*"
-    link-file "$source" "$destination"
-  done
-}
-
 # Verify all of the submodules have been updated
-function verify-submodules() {
+function verify_submodules() {
   local IFS=$'\n'
   local failed=false
   local submodules=($(sed -n -e 's/^.*path = //p' $DOTFILES_DIR/.gitmodules))
@@ -117,11 +64,10 @@ function verify-submodules() {
 }
 
 # Verify that all the files in the project actually exist
-function verify-install() {
+function verify_install() {
   echo -e "${UNDR}Verifying Install${ENDL}"
-  local files=("${TERMINAL_LIST[@]}" "${DESKTOP_LIST[@]}")
 
-  for i in ${files[@]}; do
+  for i in ${FILE_LIST[@]}; do
     local source="${i%%:*}"
     if [ -f "$source" ]; then
       echo -e "(${GREEN}File${ENDL})    $source"
@@ -133,44 +79,29 @@ function verify-install() {
     fi
   done
 
-  verify-submodules
+  verify_submodules
 }
 
-# Install all the files for the desktop environment
-function desktop() {
-  mkdir -p $HOME/.config/dunst
-
-  link-file-list 'DESKTOP_LIST'
-}
-
-# Install all the files for the terminal environment.
-function terminal() {
+# Install all the files.
+function install() {
   mkdir -p $HOME/.config/nvim
 
-  link-file-list 'TERMINAL_LIST'
+  for file in "${FILE_LIST[@]}"; do
+    local source="${file%%:*}"
+    local destination="${file##*:}"
+    ln -sfv "$source" "$destination" | sed "s/'//g"
+  done
 
   # Create tmp vim directories
-  # mkdir -p $HOME/.config/tmp/{backup,swap,undo,view}
-}
-
-# Install all the files for the both the terminal and desktop environment.
-function all() {
-  desktop
-  terminal
+  mkdir -p $HOME/.config/tmp/{backup,swap,undo,view}
 }
 
 echo -e "\nInstalling config version ${BOLD}$(git --git-dir="$DOTFILES_DIR/.git" rev-parse HEAD)${ENDL}\n"
 
 # Verify all of the files are cloned properly
-verify-install
+verify_install
 
-# Find the profile selection and run the corrosponding function
+# Perform the install
 echo -e "\n${UNDR}Symlinking files${ENDL}"
-for i in ${PROFILES[@]}; do
-  if [ $i = $PROFILE_SELECT ]; then
-    $PROFILE_SELECT
-    echo -e "\n${BOLD}Install Complete! Restart your terminal.${ENDL}"
-    exit 0
-  fi
-done
-echo -e "\"$PROFILE_SELECT\" is not a valid profile: ["${PROFILES[@]}"]"; exit 1
+install
+echo -e "\n${BOLD}Install Complete!${ENDL}"
